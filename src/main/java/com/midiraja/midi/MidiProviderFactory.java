2b5b2e045c0ca15421df7ce397c21fa00cd697ab
Park, Sungchul <gyumee@gmail.com>
2025-07-24 17:19:12 +0900

    conductor(checkpoint): Checkpoint end of Phase 1

---
+++ src/main/java/com/midiraja/midi/MidiProviderFactory.java
@@ -0,0 +1,21 @@
+package com.midiraja.midi;
+
+import com.midiraja.midi.os.LinuxProvider;
+import com.midiraja.midi.os.MacProvider;
+import com.midiraja.midi.os.WindowsProvider;
+
+public class MidiProviderFactory {
+    public static MidiOutProvider createProvider() {
+        String os = System.getProperty("os.name").toLowerCase();
+        
+        if (os.contains("mac")) {
+            return new MacProvider();
+        } else if (os.contains("win")) {
+            return new WindowsProvider();
+        } else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) {
+            return new LinuxProvider();
+        } else {
+            throw new UnsupportedOperationException("Unsupported OS for native MIDI: " + os);
+        }
+    }
+}
--- src/main/java/com/midraja/midi/MidiPort.java
+++
@@ -1,8 +0,0 @@
-package com.midraja.midi;
-
-public record MidiPort(int index, String name) {
-    @Override
-    public String toString() {
-        return String.format("[%d] %s", index, name);
-    }
-}
