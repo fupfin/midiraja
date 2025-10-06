90d7291e37d7ce5b9a33d30bf9848b1b51ccdcc3
Park, Sungchul <gyumee@gmail.com>
2025-10-06 16:35:58 +0900

    chore: add Homebrew and Scoop package manager templates
    - Added macOS/Linux Homebrew and Windows Scoop templates.
    - Lay foundation for automated CLI installations in CI/CD pipelines.

--- homebrew/midra.rb
+++ homebrew/midra.rb
@@ -1,31 +1,29 @@
 class Midra < Formula
-  desc "High-performance MIDI player for CLI."
-  homepage "https://github.com/YOUR_GITHUB_USERNAME/playmidi"
-  version "1.1.0"
+  desc "A blazing fast, cross-platform CLI MIDI player built with GraalVM"
+  homepage "https://github.com/sungchulpark/midiraja"
+  version "0.1.0"
   
+  # TODO: These URLs and SHAs will be dynamically updated by GitHub Actions during a release.
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/YOUR_GITHUB_USERNAME/playmidi/releases/download/v#{version}/midra-darwin-arm64-v#{version}.tar.gz"
-      sha256 "a79ab83831e7b1185ab1ebe78840bf9f7d814fe1170011091742db93dd9df870" # Update this from dist/checksum!
-    elsif Hardware::CPU.intel?
-      url "https://github.com/YOUR_GITHUB_USERNAME/playmidi/releases/download/v#{version}/midra-darwin-amd64-v#{version}.tar.gz"
-      sha256 "REPLACE_WITH_INTEL_SHA" # Update this!
+      url "https://github.com/sungchulpark/midiraja/releases/download/v#{version}/midra-macos-aarch64.tar.gz"
+      sha256 "REPLACE_ME_MACOS_ARM_SHA"
+    else
+      url "https://github.com/sungchulpark/midiraja/releases/download/v#{version}/midra-macos-x64.tar.gz"
+      sha256 "REPLACE_ME_MACOS_X64_SHA"
     end
   elsif OS.linux?
-    if Hardware::CPU.arm?
-      url "https://github.com/YOUR_GITHUB_USERNAME/playmidi/releases/download/v#{version}/midra-linux-arm64-v#{version}.tar.gz"
-      sha256 "REPLACE_WITH_LINUX_ARM_SHA" # Update this!
-    elsif Hardware::CPU.intel?
-      url "https://github.com/YOUR_GITHUB_USERNAME/playmidi/releases/download/v#{version}/midra-linux-amd64-v#{version}.tar.gz"
-      sha256 "REPLACE_WITH_LINUX_INTEL_SHA" # Update this!
-    end
+    url "https://github.com/sungchulpark/midiraja/releases/download/v#{version}/midra-linux-x64.tar.gz"
+    sha256 "REPLACE_ME_LINUX_X64_SHA"
   end
 
   def install
+    # The tarball should contain the standalone 'midra' binary
     bin.install "midra"
   end
 
   test do
-    system "#{bin}/midra", "--version"
+    # Simple test to verify the binary executes and outputs the help message
+    system "#{bin}/midra", "--help"
   end
 end
